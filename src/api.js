4ca926f0f039d4baf2bad8233e33303d1c20f16a
feroscardona <119539330+feroscardona@users.noreply.github.com>
2023-05-23 21:07:32 -0500

    api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL:"https://aluraflix.free.beeceptor.com"
+    baseURL:"https://localhost:5000/videos"
   })
 
 export const buscar =async (url,setData) => {
